f6d38e361d5cdee193a82defb077d4e4327403ff
Mustafa Can <57722514+MODLOFF@users.noreply.github.com>
2021-12-11 15:11:45 +0300

    Update settings.js

--- MODLOFF-Register/settings.js
+++ MODLOFF-Register/settings.js
@@ -1,39 +1,39 @@
 module.exports = {
     bot: {
-        botOwner: ["859464687401959445"],//Bot Sahibi İdiniz
-        botToken: "ODU2OTg0MzM2NDE1MTk1MTQ3.YNI-2w._SZiNkOMGtCO81Id3iFWINQwo0s",//Botun Tokenı 
+        botOwner: [""],//Bot Sahibi İdiniz
+        botToken: "",//Botun Tokenı 
         botPrefix: ".",//Botun Prefixi
         botStatus: "Passenger ❤️ MODLOFF",//Bot Durumu Kafanıza Göre Girin
-        mongoURL: "mongodb+srv://passengerxd31:passengerxd31@cluster0.hguhf.mongodb.net/weissregisterr?retryWrites=true&w=majority",//Mongo Url Gireceksiniz Buraya
+        mongoURL: "",//Mongo Url Gireceksiniz Buraya
     },
     roles: {
-        vipStaff: ["915324694931079204"],//Vip Hammer İd
-        registerStaff: ["915324694876520474"],//Register Hammer İd
-        manRoles: ["915324694838784028", "915324694838784027"], // erkek rolleri
-        womanRoles: ["915324694838784030", "915324694838784029"], // kız rolleri
-        unregisterRoles: ["915324694838784026"],//Kayıtsız İd
-        tagRole: "915324694876520470",//Tag Rol İd
-        vipRole: "915324694876520469",//Vip Rol İd
-        boosterRole: "915599948324683807",//Booster Rol İd
-        suspecious: "915324694503243854" // şüpheli hesap rolü
+        vipStaff: [""],//Vip Hammer İd
+        registerStaff: [""],//Register Hammer İd
+        manRoles: ["", ""], // erkek rolleri
+        womanRoles: ["", ""], // kız rolleri
+        unregisterRoles: [""],//Kayıtsız İd
+        tagRole: "",//Tag Rol İd
+        vipRole: "",//Vip Rol İd
+        boosterRole: "",//Booster Rol İd
+        suspecious: "" // şüpheli hesap rolü
     },
     channels: {
-        registerChannel: "916957095545421844",//Hosgeldin Kanalı İd
-        rulesChannel: "915324695342112842",//Kurallar Kanalı İd
-        botVoice: "915324695568609282",// Botun Sese Girecek Kanal İd
+        registerChannel: "",//Hosgeldin Kanalı İd
+        rulesChannel: "",//Kurallar Kanalı İd
+        botVoice: "",// Botun Sese Girecek Kanal İd
 
     },
     guild: {
-        guildID: "915324695568609282",//Sunuucu İd
-        tag: "Wéis", // BURAYA İSMİN BAŞINA GELECEK TAG BİRDEN ÇOK TAGINIZ VARSA AŞŞAĞIYA GİRİN
-        tagges: ["#1998", "1998", "ᵂ", "Wéis", "weis"], // BİRDEN ÇOK TAGINIZ VARSA BURAYA GİRİN ÖRNEK: #0001
-        defaultTag: "Wéis",
+        guildID: "",//Sunuucu İd
+        tag: "", // BURAYA İSMİN BAŞINA GELECEK TAG BİRDEN ÇOK TAGINIZ VARSA AŞŞAĞIYA GİRİN
+        tagges: ["", "", "", "", ""], // BİRDEN ÇOK TAGINIZ VARSA BURAYA GİRİN ÖRNEK: #0001
+        defaultTag: "",
         defaultName: "Wéis İsim | Yaş",
         suspeciousName: "Wéis Şüpheli Hesap",
     },
     emojis: {
-        yes: "915640534364725279",
-        no: "915640528358494248",
+        yes: "",
+        no: "",
         // EMOJİLERİN ID GİRCEKSİNİZ SADECE ID
     }
 
